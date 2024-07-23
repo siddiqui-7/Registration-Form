059915e629f0cf6e4e25b94e7b30196e6ea24163
siddiqui-7 <155855762+siddiqui-7@users.noreply.github.com>
2024-07-23 20:09:51 +0500

    Add files via upload

---
+++ registration/urls.py
@@ -0,0 +1,33 @@
+"""registration URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app1 import views
+from django.urls import path, include  # Ensure 'include' is imported here
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
+    path('', include('app1.urls')),  # Replace 'your_app' with your actual app name
+    
+     path('accounts/', include('allauth.urls')),
+
+
+    
+]
